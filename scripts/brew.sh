47ab616360db0690fb12e98d38c1db94d33e90aa
Enzo <enzo@neocoast.com>
2021-05-06 15:37:51 -0300

    fix: update homebrew install script

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -2,7 +2,7 @@
 
 # Check for homebrew
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update homebrew
